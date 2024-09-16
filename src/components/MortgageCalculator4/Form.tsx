73436374c875388a799b3a46ea2a7e04fa02d1e6
Milad Joodi <winbeta.com@gmail.com>
2024-09-16 22:06:44 +0330

    up

--- src/components/MortgageCalculator4/Form.tsx
+++ src/components/MortgageCalculator4/Form.tsx
@@ -1,20 +1,43 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import "./Form.css";
-
+import { AppContext } from "./context";
 
 const Form = () => {
+  const { state, dispatch } = useContext(AppContext);
+
+  // States
+  const [principal, setPrincipal] = useState<number>(5000);
+  const [loanTermInYears, setLoanTermInYears] = useState<number>(4);
+  const [loanTermInMonths, setLoanTermInMonths] = useState<number>(48);
+  const [interestRate, setInterestRate] = useState<number>(6.5);
+
+  // Functions
   const handleLoanTermYears = (e: ChangeEvent<HTMLInputElement>) => {};
   const handleLoanTermInMonth = (e: ChangeEvent<HTMLInputElement>) => {};
-  const setLoanTerms = (termInYears: number, termInMonths: number) => {};
+  const setLoanTerms = (termInYears: number, termInMonths: number) => {
+    setLoanTermInYears(termInYears);
+    setLoanTermInMonths(termInMonths);
+  };
   const calculateMonthlyPayment = () => {};
 
   return (
     <div className="form">
       <div className="form-item loan-amt">
         <label htmlFor="loanAmount">Loan amount</label>
-        <input type="number" id="loanAmount" placeholder="E.g 5000" />
-        <select>
-          <option value="unit">unit</option>
+        <input
+          type="number"
+          id="loanAmount"
+          placeholder="E.g 5000"
+          value={principal}
+          onChange={(e) => setPrincipal(Number(e.target.value))}
+        />
+        <select value={state.currency} onChange={
+          (e)=> dispatch(prevState => ({
+            ...prevState,
+            currency: e.target.value
+          }))
+        }>
+          <option value="unit">Unit</option>
           <option value="$">$</option>
           <option value="£">£</option>
           <option value="€">€</option>
@@ -24,23 +47,43 @@ const Form = () => {
       {/* Loan term in years */}
       <div className="form-item">
         <label htmlFor="loanTermYears">Loan term in years</label>
-        <input type="number" id="loanTermYears" placeholder="E.g 4" />
+        <input
+          type="number"
+          id="loanTermYears"
+          placeholder="E.g 4"
+          value={loanTermInYears}
+          onChange={(e) => handleLoanTermYears(e)}
+        />
       </div>
 
       {/* Loan term in month */}
       <div className="form-item">
         <label htmlFor="loanTermYears">Loan term in month</label>
-        <input type="number" id="loanTermYears" placeholder="E.g 48" />
+        <input
+          type="number"
+          id="loanTermYears"
+          placeholder="E.g 48"
+          value={loanTermInMonths}
+          onChange={(e) => handleLoanTermInMonth(e)}
+        />
       </div>
 
       {/* interest rate */}
       <div className="form-item">
         <label htmlFor="loanTermYears">Interest rate per year</label>
-        <input type="number" id="loanTermYears" placeholder="E.g 6.5" />
+        <input
+          type="number"
+          id="loanTermYears"
+          placeholder="E.g 6.5"
+          value={interestRate}
+          onChange={(e) => setInterestRate(Number(e.target.value))}
+        />
       </div>
 
       <div>
-        <button className="calculateBtn" onClick={calculateMonthlyPayment}>Calculate</button>
+        <button className="calculateBtn" onClick={calculateMonthlyPayment}>
+          Calculate
+        </button>
       </div>
     </div>
   );
--- src/components/MortgageCalculator4/Results.tsx
+++ src/components/MortgageCalculator4/Results.tsx
@@ -7,26 +7,30 @@ import "./Results.css";
 const Results = () => {
   const { state } = useContext(AppContext);
 
+  const getFormattedNumber = (value:number): string=>{
+    return value.toLocaleString(undefined, {maximumFractionDigits: 2});
+  }
+
   return (
     <div className="result">
       <div className="results-item">
         <div>Total principal amount to pay</div>
-        <div>$200</div>
+        <div>{state.currency} {getFormattedNumber(state.principalAmount)}</div>
       </div>
       <div className="results-item">
         <div>Total interest to pay</div>
-        <div>$50</div>
+        <div>{state.currency} {getFormattedNumber(state.interestAmount)}</div>
       </div>
       <hr></hr>
       <div className="results-item total-amt">
         <div>Total Loan amount to pay</div>
-        <div>$100</div>
+        <div>{state.currency} {getFormattedNumber(state.totalAmount)}</div>
       </div>
       <div className="monthly-payment">
         <div>Monthly payment</div>
         <div className="amount">
-          <span className="currency">$</span>
-          <span className="value">180</span>
+          <span className="currency">{state.currency}</span>
+          <span className="value">{getFormattedNumber(state.monthlyPayment)}</span>
         </div>
       </div>
     </div>
